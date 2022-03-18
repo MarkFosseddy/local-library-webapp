d3f524ff42f2bca865d4fc57356a613533a47904
Mark Fosseddy <markfosseddy@gmail.com>
2022-03-18 21:17:42 +0500

    move form to partials, clean up

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -31,7 +31,7 @@ class Genre
         return $this->name;
     }
 
-    public function setName(string $name): self
+    public function setName(?string $name): self
     {
         $this->name = ucfirst($name);
 
