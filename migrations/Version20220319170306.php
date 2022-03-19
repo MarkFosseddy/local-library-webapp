b865613372e954fdb35e24cb6577d0ff0ed9dc86
Mark Fosseddy <markfosseddy@gmail.com>
2022-03-19 22:23:24 +0500

    date of death is optional now

---
+++ migrations/Version20220319170306.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220319170306 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE author CHANGE date_of_death date_of_death DATE DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE author CHANGE date_of_death date_of_death DATE NOT NULL');
+    }
+}
